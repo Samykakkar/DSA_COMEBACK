52b25b65b1b800fe2e6fce13f175c5a56d93cbdc
Sameer <85270977+Samykakkar@users.noreply.github.com>
2024-07-19 16:41:15 +0530

    Time: 2088 ms (5.73%) | Memory: 22 MB (5.56%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,28 +1,56 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        trip=set()
+        # nums.sort()
+        # trip=set()
 
-        for i in range(len(nums)-2):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            firstnum=nums[i]
+        # for i in range(len(nums)-2):
+        #     if i>0 and nums[i]==nums[i-1]:
+        #         continue
+        #     firstnum=nums[i]
+        #     j=i+1
+        #     k=len(nums)-1
+
+        #     while j<k:
+        #         potsum=firstnum+nums[j]+nums[k]
+        #         if(potsum>0):
+        #             k=k-1
+        #         elif potsum<0:
+        #             j=j+1
+        #         else:
+        #             trip.add((firstnum,nums[j],nums[k]))
+        #             j=j+1
+        #             k=k-1
+        #             while j<k and nums[j]==nums[j-1]:
+        #                 j=j+1
+        #             while j<k and nums[k]==nums[k+1]:
+        #                 k=k-1
+        # return trip
+
+        nums.sort()
+        summ=0
+        st=set()
+        for i in range(0,len(nums)-2):
+
+            A=nums[i]
             j=i+1
             k=len(nums)-1
 
             while j<k:
-                potsum=firstnum+nums[j]+nums[k]
-                if(potsum>0):
+                if(A+nums[j]+nums[k])==0:
+                    tmp=[A,nums[j],nums[k]]
+                    tmp.sort()
+                    st.add(tuple(tmp))
                     k=k-1
-                elif potsum<0:
                     j=j+1
-                else:
-                    trip.add((firstnum,nums[j],nums[k]))
-                    j=j+1
+                elif(A+nums[j]+nums[k])>0:
                     k=k-1
-                    while j<k and nums[j]==nums[j-1]:
-                        j=j+1
-                    while j<k and nums[k]==nums[k+1]:
-                        k=k-1
-        return trip
+                    continue
+                elif(A+nums[j]+nums[k])<0:
+                    j=j+1
+                    continue
+                     
+
+        ans = [list(item) for item in st]
+        return ans
+ 
                     
